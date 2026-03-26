88f93879bfc4d2ad9a5e59e765286c074f43237d
MaciejD <macko2sv@gmail.com>
2026-03-27 00:01:37 +0100

    Add files via upload

---
+++ Welding_Cell.py
@@ -0,0 +1,28 @@
+from robodk.robolink import Robolink, ITEM_TYPE_ROBOT
+
+RDK = Robolink()
+robot_abb = RDK.Item('ABB IRB 120-3/0.6', ITEM_TYPE_ROBOT)
+
+if not robot_abb.Valid():
+    raise Exception("Nie znaleziono robota!")
+
+WELD_SPEED = 50
+TRAVEL_SPEED = 150
+
+weld_trajectory = [
+    [ 20, -30, 30, 0, 80, 0],  #lewo prawo |przod  tyl | gora dol | przechyl na bok|kat nachylenia|krecenie
+    [ 10, -30, 30, 0, 80, 0],
+    [  0, -30, 30, 0, 80, 0], 
+    [-10, -30, 30, 0, 80, 0],  
+    [-14, -30, 30, 0, 80, 0],  
+]
+
+robot_abb.setSpeed(TRAVEL_SPEED)
+robot_abb.MoveJ(robot_abb.JointsHome())
+
+robot_abb.setSpeed(WELD_SPEED)
+for joints in weld_trajectory:
+    robot_abb.MoveJ(joints)
+
+robot_abb.setSpeed(TRAVEL_SPEED)
+robot_abb.MoveJ(robot_abb.JointsHome())
